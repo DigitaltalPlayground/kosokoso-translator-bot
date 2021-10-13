d9c38382814050234a3bbda7b7701065a70db337
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-13 19:42:39 +0900

    Feat: able to use as library

--- src/index.js
+++ src/index.js
@@ -4,33 +4,35 @@ const discord = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 
-const client = new discord.Client({
-  intents: [
-    discord.Intents.FLAGS.GUILD_INTEGRATIONS
-  ]
-});
+module.exports = function(token){
+  const client = new discord.Client({
+    intents: [
+      discord.Intents.FLAGS.GUILD_INTEGRATIONS
+    ]
+  });
 
-/**
- * @type {{
- *  data: import("@discordjs/builders").SlashCommandBuilder,
- *  execute: function(import("discord.js").CommandInteraction):Promise<void>,
- *  name: string
- * }[]}
- */
-const commands = fs.readdirSync(path.join(__dirname, "./commands"), {withFileTypes: true})
-  .filter(d => d.isFile())
-  .map(d => d.name)
-  .filter(d => d.endsWith(".js"))
-  .map(d => d.slice(0, -3))
-  .map(d => require("./commands/" + d));
+  /**
+   * @type {{
+   *  data: import("@discordjs/builders").SlashCommandBuilder,
+   *  execute: function(import("discord.js").CommandInteraction):Promise<void>,
+   *  name: string
+   * }[]}
+   */
+  const commands = fs.readdirSync(path.join(__dirname, "./commands"), {withFileTypes: true})
+    .filter(d => d.isFile())
+    .map(d => d.name)
+    .filter(d => d.endsWith(".js"))
+    .map(d => d.slice(0, -3))
+    .map(d => require("./commands/" + d));
 
-client
-.on("ready", () => {
-  console.log("the bot is ready now");
-})
-.on("interactionCreate", async (interaction) => {
-  if(interaction.isCommand()){
-    await commands.find(c => c.name === interaction.commandName)?.execute(interaction)
-  }
-})
-.login(process.env.TOKEN)
+  client
+  .on("ready", () => {
+    console.log("the bot is ready now");
+  })
+  .on("interactionCreate", async (interaction) => {
+    if(interaction.isCommand()){
+      await commands.find(c => c.name === interaction.commandName)?.execute(interaction)
+    }
+  })
+  .login(token)
+};
